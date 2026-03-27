18372e2c9007b4d639c10f4e709e8615f27d04f1
Sigrid Muller <sigrid.muller.vidal@gmail.com>
2026-03-27 08:12:42 -0300

    ok

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ function mostrarTab(index) {
   tabs[index].classList.add("active");
   contenidos[index].classList.add("active");
 }
+
+let deferredPrompt;
+
+const instalarBtn = document.getElementById("instalarBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  instalarBtn.style.display = "block";
+});
+
+instalarBtn.addEventListener("click", async () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    const { outcome } = await deferredPrompt.userChoice;
+
+    if (outcome === "accepted") {
+      console.log("App instalada");
+    } else {
+      console.log("Instalación cancelada");
+    }
+
+    deferredPrompt = null;
+    instalarBtn.style.display = "none";
+  }
+});
